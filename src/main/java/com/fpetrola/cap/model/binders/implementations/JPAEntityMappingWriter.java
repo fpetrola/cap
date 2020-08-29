31b0c1f5d9787e041cf5546a61961eec7dc1d891
fer <fernando.petrola@gmail.com>
2020-08-28 22:31:06 -0300

    new JavaClassBinder

--- src/main/java/com/fpetrola/cap/model/binders/implementations/JPAEntityMappingWriter.java
+++ src/main/java/com/fpetrola/cap/model/binders/implementations/JPAEntityMappingWriter.java
@@ -3,10 +3,6 @@ package com.fpetrola.cap.model.binders.implementations;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 import com.fpetrola.cap.model.developer.ORMEntityMapping;
 import com.fpetrola.cap.model.developer.PropertyMapping;
@@ -15,28 +11,24 @@ import com.fpetrola.cap.model.source.SourceChange;
 import com.fpetrola.cap.model.source.SourceChangesListener;
 import com.github.javaparser.Position;
 import com.github.javaparser.Range;
-import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.NodeList;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.MemberValuePair;
 import com.github.javaparser.ast.expr.Name;
 import com.github.javaparser.ast.expr.NormalAnnotationExpr;
-import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.expr.StringLiteralExpr;
-import com.github.javaparser.ast.visitor.ModifierVisitor;
-import com.github.javaparser.ast.visitor.Visitable;
 
 public class JPAEntityMappingWriter {
 
-	private final JavaSourceChangesHandler javaSourceChangesHandler;
+	private final JavaClassBinder javaClassBinder;
 	private ORMEntityMapping ormEntityMapping;
 	private SourceChangesListener sourceChangesListener;
+	private JavaSourceChangesHandler javaSourceChangesHandler;
 
 	public JPAEntityMappingWriter(ORMEntityMapping ormEntityMapping, String workspacePath, SourceChangesListener sourceChangesListener) {
-		this.sourceChangesListener = sourceChangesListener;
 		javaSourceChangesHandler = new JavaSourceChangesHandler(workspacePath, ormEntityMapping.mappedClass);
+		this.sourceChangesListener = sourceChangesListener;
 		this.ormEntityMapping = ormEntityMapping;
+		javaClassBinder = new JavaClassBinder(javaSourceChangesHandler.getUri());
 	}
 
 	public void write() {
@@ -46,10 +38,14 @@ public class JPAEntityMappingWriter {
 			if (javaSourceChangesHandler.fileExists()) {
 				List<SourceChange> sourceChanges = new ArrayList<>();
 
-				javaSourceChangesHandler.addInsertionsFor(sourceChanges, cu1 -> addClassAnnotations(cu1));
+				NormalAnnotationExpr classNormalAnnotationExpr = new NormalAnnotationExpr(new Name("javax.persistence.Entity"), new NodeList<>());
+				classNormalAnnotationExpr.addPair("name", new StringLiteralExpr(ormEntityMapping.tableName));
+				javaSourceChangesHandler.addInsertionsFor(sourceChanges, cu1 -> javaClassBinder.addAnnotationToClass(cu1, classNormalAnnotationExpr, "JPA Entity detected", "")[0]);
 
 				for (PropertyMapping propertyMapping : ormEntityMapping.propertyMappings) {
-					javaSourceChangesHandler.addInsertionsFor(sourceChanges, cu1 -> addPropertiesAnnotations(cu1, propertyMapping));
+					javaSourceChangesHandler.addInsertionsFor(sourceChanges, cu1 -> javaClassBinder.addAnnotationToField(cu1, javaClassBinder.createAnnotation("javax.persistence.JoinColumn", new MemberValuePair("name", new StringLiteralExpr(propertyMapping.columnName))), propertyMapping.propertyName));
+					javaSourceChangesHandler.addInsertionsFor(sourceChanges, cu1 -> javaClassBinder.addAnnotationToField(cu1, javaClassBinder.createAnnotation("javax.persistence.ManyToOne"), propertyMapping.propertyName));
+					javaSourceChangesHandler.addInsertionsFor(sourceChanges, cu1 -> javaClassBinder.addFieldIfNotExists(cu1, "add property to match database column: " + propertyMapping.columnName, propertyMapping.propertyName, propertyMapping.typeName));
 				}
 
 				addFixAllForNow(uri, sourceChanges);
@@ -57,8 +53,7 @@ public class JPAEntityMappingWriter {
 				sourceChangesListener.fileCreation(uri, createNewJavaClassContent());
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
 
@@ -80,91 +75,4 @@ public class JPAEntityMappingWriter {
 
 		sourceChangesListener.sourceChange(uri, sourceChanges);
 	}
-
-	private SourceChange addClassAnnotations(CompilationUnit cu) {
-		String name = "javax.persistence.Entity";
-		NormalAnnotationExpr normalAnnotationExpr = new NormalAnnotationExpr(new Name(name), new NodeList<>());
-		normalAnnotationExpr.addPair("name", new StringLiteralExpr(ormEntityMapping.tableName));
-
-		SourceChange[] sourceChange = addAnnotation(cu, normalAnnotationExpr, "JPA Entity detected");
-
-		return sourceChange[0];
-	}
-
-	private SourceChange[] addAnnotation(CompilationUnit cu, NormalAnnotationExpr normalAnnotationExpr, final String message) {
-		SourceChange[] sourceChange = new SourceChange[1];
-		cu.accept(new ModifierVisitor<Void>() {
-			public Visitable visit(ClassOrInterfaceDeclaration classDeclaration, Void arg) {
-				List<SimpleName> simpleNames = classDeclaration.findAll(SimpleName.class);
-
-				String annotationClass = normalAnnotationExpr.getNameAsString();
-
-				String simpleName = annotationClass.substring(annotationClass.lastIndexOf(".") + 1);
-				NormalAnnotationExpr annotationExpr = (NormalAnnotationExpr) classDeclaration.getAnnotationByName(simpleName).orElseGet(() -> null);
-
-				if (annotationExpr == null) {
-					annotationExpr = classDeclaration.addAndGetAnnotation(simpleName);
-					classDeclaration.findAncestor(CompilationUnit.class).ifPresent(p -> p.addImport(annotationClass));
-					annotationExpr.setPairs(normalAnnotationExpr.getPairs());
-					sourceChange[0] = new SourceChange(javaSourceChangesHandler.getUri(), simpleNames.get(0).getRange().get(), message);
-				}
-
-
-				return super.visit(classDeclaration, arg);
-			}
-		}, null);
-		return sourceChange;
-	}
-
-	private SourceChange addPropertiesAnnotations(CompilationUnit cu, PropertyMapping propertyMapping) {
-		SourceChange[] sourceChange = new SourceChange[1];
-		boolean[] found = new boolean[1];
-
-		cu.accept(new ModifierVisitor<Void>() {
-
-			@Override
-			public Visitable visit(FieldDeclaration fieldDeclaration, Void arg) {
-
-				SimpleName name = fieldDeclaration.getVariable(0).getName();
-
-				if (name.toString().equals(propertyMapping.propertyName)) {
-					found[0] = true;
-
-					if (propertyMapping.propertyMappingType != null) {
-						if (!fieldDeclaration.isAnnotationPresent(ManyToOne.class)) {
-							fieldDeclaration.addAnnotation(ManyToOne.class);
-							sourceChange[0] = new SourceChange(javaSourceChangesHandler.getUri(), fieldDeclaration.getRange().get(), "column mapping detected for property: " + propertyMapping.columnName);
-						}
-					}
-
-					if (propertyMapping.columnName != null) {
-						if (!fieldDeclaration.isAnnotationPresent(JoinColumn.class)) {
-							NormalAnnotationExpr annotation = fieldDeclaration.addAndGetAnnotation(JoinColumn.class);
-							annotation.addPair("name", new StringLiteralExpr(propertyMapping.columnName));
-						}
-					}
-				}
-				return super.visit(fieldDeclaration, arg);
-			}
-		}, null);
-
-		if (!found[0]) {
-			cu.findAll(ClassOrInterfaceDeclaration.class).forEach(coid -> {
-				VariableDeclarator variables = new VariableDeclarator();
-				variables.setName(propertyMapping.propertyName);
-				variables.setType(propertyMapping.typeName.contains("INT") ? Integer.class : String.class);
-				FieldDeclaration fieldDeclaration = new FieldDeclaration().addVariable(variables);
-				coid.getMembers().add(0, fieldDeclaration);
-				Optional<String> fullyQualifiedName = coid.getFullyQualifiedName();
-				Optional<SimpleName> simpleNames = coid.findAll(SimpleName.class).stream().filter(sn -> sn.getParentNode().get() instanceof ClassOrInterfaceDeclaration).findFirst();
-
-				simpleNames.ifPresent(p -> {
-					sourceChange[0] = new SourceChange(javaSourceChangesHandler.getUri(), p.getRange().get(), "add property to match database column: " + propertyMapping.columnName);
-				});
-
-			});
-		}
-		return sourceChange[0];
-	}
-
 }
---
+++ src/main/java/com/fpetrola/cap/model/binders/implementations/JavaClassBinder.java
@@ -0,0 +1,113 @@
+package com.fpetrola.cap.model.binders.implementations;
+
+import java.util.Optional;
+
+import com.fpetrola.cap.model.source.SourceChange;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.MemberValuePair;
+import com.github.javaparser.ast.expr.Name;
+import com.github.javaparser.ast.expr.NormalAnnotationExpr;
+import com.github.javaparser.ast.expr.SimpleName;
+import com.github.javaparser.ast.visitor.ModifierVisitor;
+import com.github.javaparser.ast.visitor.Visitable;
+
+public class JavaClassBinder {
+	private String uri;
+
+	public JavaClassBinder(String uri) {
+		this.uri = uri;
+	}
+
+	protected NormalAnnotationExpr createAnnotation(String identifier, MemberValuePair... memberValuePair) {
+		return new NormalAnnotationExpr(new Name(identifier), NodeList.nodeList(memberValuePair));
+	}
+
+	protected SourceChange[] addAnnotationToClass(CompilationUnit cu, NormalAnnotationExpr normalAnnotationExpr, final String message, String name) {
+		SourceChange[] sourceChange = new SourceChange[1];
+		cu.accept(new ModifierVisitor<Void>() {
+			public Visitable visit(ClassOrInterfaceDeclaration classDeclaration, Void arg) {
+				sourceChange[0] = addAnnotationToBodyDeclaration(normalAnnotationExpr, message, classDeclaration, classDeclaration.findAll(SimpleName.class).get(0));
+				return super.visit(classDeclaration, arg);
+			}
+		}, null);
+		return sourceChange;
+	}
+
+	protected SourceChange addAnnotationToField(CompilationUnit cu, NormalAnnotationExpr normalAnnotationExpr, final String propertyName) {
+		SourceChange[] sourceChange = new SourceChange[1];
+		cu.accept(new ModifierVisitor<Void>() {
+			public Visitable visit(FieldDeclaration fieldDeclaration, Void arg) {
+				SimpleName fieldName = fieldDeclaration.getVariable(0).getName();
+				if (fieldName.toString().equals(propertyName)) {
+					String message = "add annotation " + normalAnnotationExpr.getNameAsString() + " to property: " + propertyName;
+					sourceChange[0] = addAnnotationToBodyDeclaration(normalAnnotationExpr, message, fieldDeclaration, fieldDeclaration);
+				}
+
+				return super.visit(fieldDeclaration, arg);
+			}
+		}, null);
+		return sourceChange[0];
+	}
+
+	protected SourceChange addFieldIfNotExists(CompilationUnit cu, String message, String propertyName, String typeName) {
+		boolean found = fieldExists(cu, propertyName);
+
+		SourceChange[] sourceChange = new SourceChange[1];
+		if (!found) {
+			cu.findAll(ClassOrInterfaceDeclaration.class).forEach(coid -> {
+				VariableDeclarator variables = new VariableDeclarator();
+				variables.setName(propertyName);
+				variables.setType(typeName.contains("INT") ? Integer.class : String.class);
+				FieldDeclaration fieldDeclaration = new FieldDeclaration().addVariable(variables);
+				coid.getMembers().add(0, fieldDeclaration);
+				Optional<String> fullyQualifiedName = coid.getFullyQualifiedName();
+				Optional<SimpleName> simpleNames = coid.findAll(SimpleName.class).stream().filter(sn -> sn.getParentNode().get() instanceof ClassOrInterfaceDeclaration).findFirst();
+
+				simpleNames.ifPresent(p -> {
+					sourceChange[0] = new SourceChange(uri, p.getRange().get(), message);
+				});
+
+			});
+		}
+		return sourceChange[0];
+	}
+
+	private SourceChange addAnnotationToBodyDeclaration(NormalAnnotationExpr normalAnnotationExpr, final String message, BodyDeclaration classDeclaration, Node node) {
+		SourceChange[] sourceChange = new SourceChange[1];
+
+		if (node != null) {
+			String annotationClass = normalAnnotationExpr.getNameAsString();
+
+			String simpleName = annotationClass.substring(annotationClass.lastIndexOf(".") + 1);
+			NormalAnnotationExpr annotationExpr = (NormalAnnotationExpr) classDeclaration.getAnnotationByName(simpleName).orElseGet(() -> null);
+
+			if (annotationExpr == null) {
+				annotationExpr = classDeclaration.addAndGetAnnotation(simpleName);
+				classDeclaration.findAncestor(CompilationUnit.class).ifPresent(p -> p.addImport(annotationClass));
+				annotationExpr.setPairs(normalAnnotationExpr.getPairs());
+				sourceChange[0] = new SourceChange(uri, node.getRange().get(), message);
+			}
+		}
+		return sourceChange[0];
+	}
+
+	private boolean fieldExists(CompilationUnit cu, final String propertyName) {
+		boolean[] found = new boolean[1];
+		cu.accept(new ModifierVisitor<Void>() {
+			public Visitable visit(FieldDeclaration fieldDeclaration, Void arg) {
+				SimpleName fieldName = fieldDeclaration.getVariable(0).getName();
+				if (fieldName.toString().equals(propertyName)) {
+					found[0] = true;
+				}
+				return super.visit(fieldDeclaration, arg);
+			}
+		}, null);
+		return found[0];
+	}
+}
